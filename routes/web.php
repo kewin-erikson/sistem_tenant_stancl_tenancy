150a23fde70742ef7f1990ad7ce132d47a092dd1
kewin-erikson <kewinerikson@gmail.com>
2026-01-14 17:18:22 -0500

    aaa

--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Livewire::setScriptRoute(function ($handle) {
-//     // Obtiene la URL base de la aplicación desde el archivo .env
-//     $baseUrl = config('app.url');
+Livewire::setScriptRoute(function ($handle) {
+    // Obtiene la URL base de la aplicación desde el archivo .env
+    $baseUrl = config('app.url');
     
-//     // Analiza la URL para extraer el segmento de la ruta si existe
-//     $parsedUrl = parse_url($baseUrl);
-//     $subfolder = isset($parsedUrl['path']) ? rtrim($parsedUrl['path'], '/') : '';
+    // Analiza la URL para extraer el segmento de la ruta si existe
+    $parsedUrl = parse_url($baseUrl);
+    $subfolder = isset($parsedUrl['path']) ? rtrim($parsedUrl['path'], '/') : '';
 
-//     // Construye la ruta del script Livewire basándose en la presencia del segmento
-//     $scriptRoute = $subfolder ? $subfolder . '/public/livewire/livewire.js' : '/public/livewire/livewire.js';
+    // Construye la ruta del script Livewire basándose en la presencia del segmento
+    $scriptRoute = $subfolder ? $subfolder . '/public/livewire/livewire.js' : '/public/livewire/livewire.js';
 
-//     // Devuelve la ruta con el manejador de ruta proporcionado (Route::get)
-//     return Route::get($scriptRoute, $handle);
-// });
+    // Devuelve la ruta con el manejador de ruta proporcionado (Route::get)
+    return Route::get($scriptRoute, $handle);
+});
 
-// Livewire::setUpdateRoute(function ($handle) {
-//     // Obtiene la URL base de la aplicación desde el archivo .env
-//     $baseUrl = config('app.url');
+Livewire::setUpdateRoute(function ($handle) {
+    // Obtiene la URL base de la aplicación desde el archivo .env
+    $baseUrl = config('app.url');
     
-//     // Analiza la URL para extraer el segmento de la ruta si existe
-//     $parsedUrl = parse_url($baseUrl);
-//     $subfolder = isset($parsedUrl['path']) ? rtrim($parsedUrl['path'], '/') : '';
+    // Analiza la URL para extraer el segmento de la ruta si existe
+    $parsedUrl = parse_url($baseUrl);
+    $subfolder = isset($parsedUrl['path']) ? rtrim($parsedUrl['path'], '/') : '';
 
-//     // Construye la ruta de actualización de Livewire basándose en la presencia del segmento
-//     $updateRoute = $subfolder ? $subfolder . '/public/livewire/update' : '/public/livewire/update';
+    // Construye la ruta de actualización de Livewire basándose en la presencia del segmento
+    $updateRoute = $subfolder ? $subfolder . '/public/livewire/update' : '/public/livewire/update';
 
-//     // Devuelve la ruta con el manejador de ruta proporcionado (Route::post)
-//     return Route::post($updateRoute, $handle)->name('livewire.update.custom');
-// });
+    // Devuelve la ruta con el manejador de ruta proporcionado (Route::post)
+    return Route::post($updateRoute, $handle)->name('livewire.update.custom');
+});
 
 Route::get('/', function () {
     return view('welcome');
